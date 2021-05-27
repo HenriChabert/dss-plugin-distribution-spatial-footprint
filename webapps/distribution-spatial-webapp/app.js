7c936edf618aa8874b9a480413fd6fbe5e87a08d
admin <admin>
2021-05-27 19:11:21 +0200

    added custom webapp

---
+++ webapps/distribution-spatial-webapp/app.js
@@ -0,0 +1,5 @@
+// Access the parameters that end-users filled in using webapp config
+// For example, for a parameter called "input_dataset"
+// input_dataset = dataiku.getWebAppConfig()['input_dataset']
+
+import "./spatial-footprint-app/app.js";
