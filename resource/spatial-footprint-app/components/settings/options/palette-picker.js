c0ea263398e08334531073534c90a4d6fe09f46d
HenriChabert <henri@famille-chabert.com>
2021-05-28 11:19:33 +0200

    Separated available palettes

--- resource/spatial-footprint-app/components/settings/options/palette-picker.js
+++ resource/spatial-footprint-app/components/settings/options/palette-picker.js
@@ -1,43 +1,47 @@
 import { DKUApi } from '../../../dku-api.js'
 
-const BUILT_IN_PALETTES = [
-    {
-        value: "dku_dss_next",
-        label: "DSS Next",
-        colors: ["#00AEDB", "#8CC63F", "#FFC425", "#F37735", "#D11141", "#91268F", "#194BA3", "#00B159"]
-    },
-    {
-        value: 'dku_pastel1',
-        label: "Pastel",
-        colors: ["#EC6547", "#FDC665", "#95C37B", "#75C2CC", "#694A82", "#538BC8", "#65B890", "#A874A0"]
-    },
-    {
-        value: 'dku_corpo1',
-        label: "Corporate",
-        colors: ["#0075B2", "#818991", "#EA9423", "#A4C2DB", "#EF3C39", "#009D4B", "#CFD6D3", "#231F20"]
-    },
-    {
-        value: 'dku_deuteranopia1',
-        label: "Deuterano",
-        colors: ["#193C81", "#7EA0F9", "#211924", "#757A8D", "#D6C222", "#776A37", "#AE963A", "#655E5D"]
-    },
-    {
-        value: 'dku_tritanopia1',
-        label: "Tritanopia",
-        colors: ["#CA0849", "#0B4D61", "#E4B2BF", "#3F6279", "#F24576", "#7D8E98", "#9C4259", "#2B2A2E"]
-    },
-    {
-        value: 'dku_pastel2',
-        label: 'Pastel 2',
-        colors: ["#f06548", "#fdc766", "#7bc9a6", "#4ec5da", "#548ecb", "#97668f", "#5e2974"]
-    }
-]
+const BUILT_IN_PALETTES = {
+    location: [
+        {
+            value: "dku_dss_next",
+            label: "DSS Next",
+            colors: ["#00AEDB", "#8CC63F", "#FFC425", "#F37735", "#D11141", "#91268F", "#194BA3", "#00B159"]
+        },
+        {
+            value: 'dku_pastel1',
+            label: "Pastel",
+            colors: ["#EC6547", "#FDC665", "#95C37B", "#75C2CC", "#694A82", "#538BC8", "#65B890", "#A874A0"]
+        },
+        {
+            value: 'dku_corpo1',
+            label: "Corporate",
+            colors: ["#0075B2", "#818991", "#EA9423", "#A4C2DB", "#EF3C39", "#009D4B", "#CFD6D3", "#231F20"]
+        },
+        {
+            value: 'dku_pastel2',
+            label: 'Pastel 2',
+            colors: ["#f06548", "#fdc766", "#7bc9a6", "#4ec5da", "#548ecb", "#97668f", "#5e2974"]
+        }
+    ],
+    competitor: [
+        {
+            value: 'dku_deuteranopia1',
+            label: "Deuterano",
+            colors: ["#193C81", "#7EA0F9", "#211924", "#757A8D", "#D6C222", "#776A37", "#AE963A", "#655E5D"]
+        },
+        {
+            value: 'dku_tritanopia1',
+            label: "Tritanopia",
+            colors: ["#CA0849", "#0B4D61", "#E4B2BF", "#3F6279", "#F24576", "#7D8E98", "#9C4259", "#2B2A2E"]
+        }
+    ]
+}
 
 const PalettePicker = {
     name: "palette-picker",
     data() {
         return {
-            options: BUILT_IN_PALETTES,
+            options: BUILT_IN_PALETTES[this.settingsModule],
         }
     },
     props: {
