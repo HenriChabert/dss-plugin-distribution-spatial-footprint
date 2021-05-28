eecd541b6642fd1930412831ffac17b053039212
HenriChabert <henri@famille-chabert.com>
2021-05-28 09:11:52 +0200

    All init commit

--- webapps/distribution-spatial-webapp/app.js
+++ webapps/distribution-spatial-webapp/app.js
@@ -1,5 +1,1 @@
-// Access the parameters that end-users filled in using webapp config
-// For example, for a parameter called "input_dataset"
-// input_dataset = dataiku.getWebAppConfig()['input_dataset']
-
-import "./spatial-footprint-app/app.js";
+import "../../resource/spatial-footprint-app/app.js";
