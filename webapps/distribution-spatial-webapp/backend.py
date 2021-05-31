d87b44696ba8695d70a5d75ddfce4f97dc39566b
HenriChabert <henri@famille-chabert.com>
2021-05-31 18:30:18 +0200

    Removed app ddef

--- webapps/distribution-spatial-webapp/backend.py
+++ webapps/distribution-spatial-webapp/backend.py
@@ -1,10 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask
 from distributionspatialfootprint.api import define_endpoints
-from flask_cors import CORS
-
-app = Flask(__name__)
-cors = CORS(app)
 
 define_endpoints(app)
